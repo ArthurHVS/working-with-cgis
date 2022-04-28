af95295b9fb7df1ce42e83dea28306684e7e9511
Arthur Henrique <arthur.henrique@mblabs.com.br>
2022-04-28 01:17:45 -0300

    chore: README and argv

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var { argv } = require('process');
 var cgi = require('cgi');
 var http = require('http');
 var path = require('path');
- 
-var script = path.resolve(__dirname, './cgi', 'test-cgi');
- 
-http.createServer( cgi(script) ).listen(8080);
+
+var script = path.resolve(__dirname, './cgi', 'test.cgi');
+
+http.createServer(cgi(script)).listen(argv[2]);
